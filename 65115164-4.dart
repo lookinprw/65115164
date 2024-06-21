7d132626fc7740401091ef912967e81f29921374
lookinprw <140128880+lookinprw@users.noreply.github.com>
2024-06-21 11:55:00 +0700

    Create 65115164-4.dart

---
+++ 65115164-4.dart
@@ -0,0 +1,19 @@
+int factorial(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+void main() {
+  int n = 5; 
+ 
+  if (n < 0) {
+    print('Please enter a valid positive integer.');
+    return;
+  }
+  int result = factorial(n);
+
+  print('The factorial of $n is: $result');
+}
